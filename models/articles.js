60fa1678831b027f1fa5d895382a41d7b02e378b
dogray7722 <david.gray61@du.edu>
2017-09-17 08:48:02 -0600

    Rewire to use express routing engine. Put routes in separate file.

---
+++ models/articles.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    headline: {type: String, required: true},
+    summary: {type: String, required: true},
+    URL: {type: String, required: true},
+    saved: false
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express require('express');
-const mongoose require('mongoose');
-const bodyParser require('body-parser');
-const cheerio require('cheerio');
-const rqeuest require('request');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+const request = require('request');
+const articles = require('./models/articles');
+const notes = require('./models/notes');
+const path = require('path');
 
+//Use Javascript ES6 Mongoose Promises
+mongoose.Promise = Promise;
+
+//Flexible port logic so we can ship the app to heroku
 const PORT = process.env.PORT || 8090;
 
 const app = express();
@@ -16,6 +23,55 @@ const exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+//Connect to mongoscraper database
+
+mongoose.connect("mongodb://localhost/mongoscraper");
+var db = mongoose.connection;
+
+//Show any mongoose errors or notify of successful connection
+db.on("error", function(error){
+    console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+   console.log("Mongoose connection successful.");
+});
+
+//R
+
+
+
+//Initial Test routes:  These will be moved into the routes directory once working
+
+app.get('/', function(req, res) {
+    res.render('index')
+});
+
+
+app.get('/articles', function(req, res) {
+    articles.find({}, function(err, doc){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.json(doc);
+        }
+    });
+});
+
+app.get('/saved', function(req, res) {
+    articles.find({saved: true}, function(err, doc){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.json(doc);
+        }
+    });
+});
+
+
 
 app.listen(PORT, function() {
     console.log('Listening on port ' + PORT);
