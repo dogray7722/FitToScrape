60a76560da2bd17bb32606012872697db3a56550
dogray7722 <david.gray61@du.edu>
2017-09-17 21:52:28 -0600

    Move script file out of main.handlebars file.

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,4 +1,14 @@
+$(".button-collapse").sideNav();
+$(document).ready(function(){
+    $('.slider').slider({
+        indicators: false
+    });
+    $('.modal').modal();
+
+});
+
 $("#saved").on("click", function(event){
     event.preventDefault();
     window.location.href = "/saved"
 });
+
