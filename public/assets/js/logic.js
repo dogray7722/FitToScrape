7aaab654b53055bd306ea662be445ea4c237f8b4
dogray7722 <david.gray61@du.edu>
2017-09-19 23:01:20 -0600

    Add comments to logic.js file.

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,18 +1,20 @@
+
+//Materialize initiators
 $(".button-collapse").sideNav();
 $(document).ready(function(){
     $('.slider').slider({
         indicators: false
     });
     $('.modal').modal();
-
 });
 
+//On click of the saved articles button send render the saved handlebars templated
 $("#saved").on("click", function(event){
     event.preventDefault();
     window.location.href = "/saved"
 });
 
-
+//When clicking the save button for an article, make a put request to toggle the saved boolean
 $(".savebutton").on("click", function(event){
     event.preventDefault();
     $.ajax({
@@ -22,7 +24,7 @@ $(".savebutton").on("click", function(event){
     window.location.href = "/"
 });
 
-
+//When clicking the delete button, toggle the saved boolean again so
 $(".delete").on("click", function(event){
     event.preventDefault();
     console.log($(this).attr("data-id"));
