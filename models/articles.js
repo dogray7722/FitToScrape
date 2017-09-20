755333bad27f671875a17dc9d9c0b4c1b9e615dc
dogray7722 <david.gray61@du.edu>
2017-09-19 22:48:05 -0600

    Create note schema.

--- models/articles.js
+++ models/articles.js
@@ -5,7 +5,8 @@ var ArticleSchema = new Schema({
     headline: {type: String, required: true},
     summary: {type: String, required: true},
     link: {type: String, required: true},
-    saved: false
+    saved: false,
+    note: {type: Schema.Types.ObjectId, ref: "Note"}
 });
 
 var Article = mongoose.model("Article", ArticleSchema);
